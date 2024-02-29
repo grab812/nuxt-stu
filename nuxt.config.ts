e8f609f58f404eded288e94531cb94ed0b582315
이아람 <aram.lee5@cj.net>
2024-02-29 14:09:37 +0900

    reset&default css 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { createResolver } from "@nuxt/kit";
+const { resolve } = createResolver(import.meta.url);
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   components: [
@@ -7,4 +10,37 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
+  app: {
+    head: {
+      // Default meta
+      htmlAttrs: { lang: "ko" },
+      viewport:
+        "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0",
+      title: "CJ ENM AD Portal IMFFECT C",
+      link: [{ rel: "icon", href: "/favicon.svg" }],
+      meta: [
+        { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+        { property: "og:site_name", content: "IMFFECT C" },
+        { property: "og:type", content: "website" },
+        { property: "og:title", content: "CJ ENM AD Portal IMFFECT C" },
+        {
+          property: "og:description",
+          content:
+            "CJ ENM의 통합마케팅솔루션, 최적의 캠페인으로 광고주와 함께 합니다.",
+        },
+        {
+          property: "og:keywords",
+          content:
+            "tvN, Tving, Mnet, OCN, tvN Story, tvN Thrills, tvN Show, Olive, DIA TV, Tooniverse, 투니버스, CJ ENM, 방송광고, TV광고, 동영상광고, 디지털광고, 온라인광고, 방송 캠페인, 디지털 캠페인, 캠페인 집행, On-Off 이벤트, 프로모션, 디지털 스튜디오, 인플루언서 광고",
+        },
+        {
+          property: "og:image",
+          content: "https://imffectc.cjenm.com/kakaoshare_image.png",
+        },
+        { property: "og:url", content: "https://imffectc.cjenm.com" },
+        { charset: "utf-8" },
+      ],
+    },
+  },
+  css: [resolve("/assets/scss/_index.scss")],
 });
